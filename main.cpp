7bbcb3f9cae08789ed367337b7c45b558724ae05
Melissa Quinnan <mkq5008@gmail.com>
2025-03-13 16:16:17 +0100

    update

--- main.cpp
+++ main.cpp
@@ -10,12 +10,36 @@ int main()
   std::shared_ptr<hls4mlEmulator::Model> model = loader.load_model();
 
   std::cout << "modelInput = [";
-  ap_fixed<18,13> modelInput[57];
+  ap_fixed<18,13> modelInput[57] = {2047, 0, 0, 255, 20, 140, 10, 8, 77, 7, -4, 62, 5, 34, 73, 4, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1023, 19, 139, 234, 5, 71, 49, 63, 55, 44, 41, 7, 25, -86, 107, 13, 37, 121, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
   for (int i = 0; i < 57; i++) {
-    modelInput[i] = 10.25;
+    // modelInput[i] = 10.25;
     std::cout << modelInput[i] << ", ";
   }
   std::cout << "]" << std::endl;
+
+  ap_fixed<14,6,AP_RND_CONV,AP_SAT> scaled[57];
+  ap_fixed<18,13> unscaled[57];
+  typedef ap_fixed<5,5> ad_shift_t;
+  typedef ap_fixed<10,10> ad_offset_t;
+
+  const ad_shift_t ad_shift[57] = {3, 0, 6, 2, 5, 6, 0, 5, 6, 0, 5, 6, -1, 5, 6, 2, 7, 8, 0, 7, 8, 0, 7, 8, 0, 7, 8, 4, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 3, 6, 6, 2, 6, 6};
+  const ad_offset_t ad_offsets[57] = {18, 0, 72, 7, 0, 73, 4, 0, 73, 4, 0, 72, 3, 0, 72, 6, -0, 286, 3, -2, 285, 3, -2, 282, 3, -2, 286, 29, 0, 72, 22, 0, 72, 18, 0, 72, 14, 0, 72, 11, 0, 72, 10, 0, 72, 10, 0, 73, 9, 0, 73, 9, 0, 72, 8, -2, 72};
+ 
+  for (int i = 0; i < 57; i++)
+    {
+      ap_fixed<18,13> tmp0 = unscaled[i] - ad_offsets[i];
+      ap_fixed<14,6,AP_RND_CONV,AP_SAT> tmp1 = tmp0 >> ad_shift[i];
+      scaled[i] = tmp1;
+    }
+
+  std::cout << "scaledInput = [";
+  for (int i = 0; i < 57; i++) {
+    // modelInput[i] = 10.25;
+    std::cout << scaled[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+
   
   ap_fixed<18,14,AP_RND_CONV,AP_SAT> modelResult; //changed for v5
   // std::array<ap_fixed<18,14,AP_RND_CONV,AP_SAT>,1> modelResult; //changed for v5
@@ -60,7 +84,6 @@ int main()
 
   std::cout << pairResult.first << " " << pairResult.second << std::endl;
   
-
   return 0;
 }
  
